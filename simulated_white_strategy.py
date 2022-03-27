76eb94111201acccac235869e719e1e288e085a9
bognaj <249811@student.pwr.edu.pl>
2022-03-27 19:55:07 +0200

    first results

--- simulated_white_strategy.py
+++ simulated_white_strategy.py
@@ -14,15 +14,13 @@ def white_wins(whites, blacks):
     else:
         return False
 
-def one_case(first_decision, barrier, n):
+def one_case_classic(first_decision, barrier, n):
     X = generate_season_scores(n)
 
     whites_result = X[first_decision-1]
-
     for_black = np.array(X[first_decision:])
     powers = list(range(len(for_black), 0, -1))
-
-    raised = np.power(1 - np.abs(for_black - barrier), powers)
+    raised =  1 - np.abs(for_black - barrier)
 
     try:
         b = np.where(raised > whites_result)[0][0]
@@ -34,41 +32,79 @@ def one_case(first_decision, barrier, n):
         except:
             return('w', whites_result)
 
-dict_white = {k: [] for k in range(2, 100)}
-dict_black_standard = {k: [] for k in range(2, 100)}
-dict_black_barrier = {k: [] for k in range(2, 100)}
+
+def one_case_all_power(first_decision, barrier, n):
+    X = generate_season_scores(n)
+
+    whites_result = X[first_decision-1]
+    for_black = np.array(X[first_decision:])
+    powers = list(range(len(for_black), 0, -1))
+    raised =  np.power(1 - np.abs(for_black - barrier), powers)
+
+    try:
+        b = np.where(raised > whites_result)[0][0]
+        return('bb', for_black[b])
+    except:
+        try:
+            b = np.where(for_black > whites_result)[0][0]
+            return('bs', for_black[b])
+        except:
+            return('w', whites_result)
+
+def one_case_old(first_decision, barrier, n):
+    X = generate_season_scores(n)
+
+    whites_result = X[first_decision-1]
+    for_black = np.array(X[first_decision:])
+ 
+    try:
+        b = np.where(for_black > whites_result)[0][0]
+        return('bs', for_black[b])
+    except:
+        return('w', whites_result)
+
+dict_classic = {k: [] for k in range(2, 100)}
+dict_all_power = {k: [] for k in range(2, 100)}
+dict_old= {k: [] for k in range(2, 100)}
 
 for b in [0.5]:
-    for i in range(1000):
+    for i in range(10000):
         for j in range(2, 100):
             print(b, i, j)
-            x = one_case(100 - j + 1, b, 100)
+            x = one_case_classic(100 - j + 1, b, 100)
+            y = one_case_all_power(100 - j + 1, b, 100)
+            z = one_case_old(100 - j + 1, b, 100)
             if x[0] == 'w':
-                dict_white[j].append(x[1])
-            elif x[0] == 'bb':
-                dict_black_barrier[j].append(x[1])
-            else:
-                dict_black_standard[j].append(x[1])
+                dict_classic[j].append(x[1])
+            if y[0] == 'w':
+                dict_all_power[j].append(y[1])
+            if z[0] == 'w':
+                dict_old[j].append(z[1])
 
+                
 ns = list(range(2, 100))
-vals_white = []
-vals_black_barrier = []
-vals_black_standard = []
+classic = []
+all_power = []
+old = []
 
 for i in ns:
-    val_list = dict_white[i]
+    val_list = dict_classic[i]
     val = np.quantile(val_list, 0.5)
-    vals_white.append(val)
+    classic.append(val)
 
-    val_list = dict_black_barrier[i]
+    val_list = dict_all_power[i]
     val = np.quantile(val_list, 0.5)
-    vals_black_barrier.append(val)
+    all_power.append(val)
 
-    val_list = dict_black_standard[i]
+    val_list = dict_old[i]
     val = np.quantile(val_list, 0.5)
-    vals_black_standard.append(val)
+    old.append(val)
 
-plt.scatter(ns, vals_white)
-plt.scatter(ns, vals_black_barrier)
-plt.scatter(ns, vals_black_standard)
+
+plt.scatter(ns, classic, label = 'classic')
+plt.scatter(ns, all_power, label = 'all power')
+plt.scatter(ns, old, label = 'old')
+#plt.scatter(ns, vals_black_barrier)
+#plt.scatter(ns, vals_black_standard)
+plt.legend()
 plt.show()
